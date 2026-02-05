02d03a910f0efcf14013b3218c5fe92ac66eeb1f
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-02-05 15:51:58 +0000

    added a loop test

--- P1LAB1_Behagg.py
+++ P1LAB1_Behagg.py
@@ -3,6 +3,10 @@
 #P1LAB1
 #A short python assignment to use inputs and outputs
 
-first_name = input("Enter your first name:")
-last_name = input("Enter your last name:")
-print("hello,", first_name, last_name + "!", "Welcome to CTI-110")
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("hello,", first_name, last_name + "!", "Welcome to CTI-110 😊")
+
+count = int(input("how many repetitions "))
+for i in range(count):
+    print(first_name, last_name)
