d45ee9233d8c3a6efb44f60de36a09ff24de8e01
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-02-05 18:35:09 +0000

    Added a screenshot and added logic to the calculator

--- P1HW1/P1HW1_behagg.py
+++ P1HW1/P1HW1_behagg.py
@@ -1,3 +1,6 @@
+#Parker Behagg 
+#2/5/2026
+#P1HW1
 # simple calculator
 
 operation_type = input("which would you like to calculate, Exponents or Additon? ")
@@ -8,9 +11,27 @@ if operation_type == "Addition" or operation_type == "addition":
     base_number = int(input("what is the base number (number to add to or subtract from): "))
     add_number = int(input("what is the number to add? (enter '0' to skip)" ))
     sub_number = int(input("what is the number to subtract? (enter '0' to skip)" ))
-    print("base:", base_number, "add:", add_number,"sub:", sub_number)
+    result = base_number + add_number - sub_number
+    print("base:", base_number, "add:", add_number,"sub:", sub_number, "=", result)
 elif operation_type == "Exponents" or operation_type == "exponents" or operation_type == "Exponent" or operation_type == "exponent":
     print("operation selected: Exponent")
     base_number = int(input("what is the base number (number to raise): "))
     exponent_number = int(input("what is the power to raise the base to?"))
-    print("base:", base_number, "exp:", exponent_number)
+    result = pow(base_number, exponent_number)
+    print("base:", base_number, "exp:", exponent_number, "=", result)
+elif operation_type == "both" or operation_type == "Both":
+    print()
+    print("----Addition----")
+    print()
+    base_number = int(input("what is the base number (number to add to or subtract from): "))
+    add_number = int(input("what is the number to add? (enter '0' to skip)" ))
+    sub_number = int(input("what is the number to subtract? (enter '0' to skip)" ))
+    result = base_number + add_number - sub_number
+    print("base:", base_number, "add:", add_number,"sub:", sub_number, "=", result)
+    print()
+    print("----Exponents----")
+    print()
+    base_number = int(input("what is the base number (number to raise): "))
+    exponent_number = int(input("what is the power to raise the base to?"))
+    result = pow(base_number, exponent_number)
+    print("base:", base_number, "exp:", exponent_number, "=", result)
