b5eee8d07411811e50338492b89af0c0717efaee
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-02-10 16:06:24 +0000

    Updated the calculator to be able to restart and handle invalid inputs better

--- P1HW1/P1HW1_behagg.py
+++ P1HW1/P1HW1_behagg.py
@@ -3,37 +3,66 @@
 #P1HW1
 # simple calculator
 
-operation_type = input("which would you like to calculate, Exponents or Additon? ")
+#----define the calculator as a function to allow re-running without restarting the file----
+def calculator():
+ #----promtp the user for the type of calculation they want to perform----
+    operation_type = input("which would you like to calculate, Exponents or Additon? ")
+ #----use if statements to determine which type of calculation the user wants to perform----
+    if operation_type.lower() == "addition":                                                                            #addition and subtraction
+        print("operation selected: Addition")
+        print()
+        base_number = float(input("what is the base number (number to add to or subtract from): "))
+        add_number = float(input("what is the number to add? (enter '0' to skip)" ))
+        sub_number = float(input("what is the number to subtract? (enter '0' to skip)" ))
+        result = base_number + add_number - sub_number
+        print("base:", base_number, "add:", add_number,"sub:", sub_number, "=", result)
+        repeat_calculator()
+    elif operation_type.lower() == "exponents" or operation_type.lower() == "exponent":                                 #exponnents
+        print("operation selected: Exponent")
+        base_number = float(input("what is the base number (number to raise): "))
+        exponent_number = float(input("what is the power to raise the base to: "))
+        result = pow(base_number, exponent_number)
+        print("base:", base_number, "exp:", exponent_number, "=", result, "Integer form:", int(result))
+        repeat_calculator()
+    elif operation_type.lower() == "both":                                                                              #calculate both starting with addition and then exponents
+        print("operation selected: Both")
+        print()
+        print("----Addition----")
+        print()
+        base_number = float(input("what is the base number (number to add to or subtract from): "))
+        add_number = float(input("what is the number to add? (enter '0' to skip) " ))
+        sub_number = float(input("what is the number to subtract? (enter '0' to skip) " ))
+        result = base_number + add_number - sub_number
+        print("base:", base_number, "add:", add_number,"sub:", sub_number, "=", result)
+        print()
+        print("----Exponents----")
+        print()
+        base_number = float(input("what is the base number (number to raise): "))
+        exponent_number = float(input("what is the power to raise the base to: "))
+        result = pow(base_number, exponent_number)
+        print("base:", base_number, "exp:", exponent_number, "=", result, "Integer form:", int(result))
+        repeat_calculator()
+    elif operation_type == "Exit" or operation_type == "exit":                                                          #exit the calculator
+        print("calculator closed.")
+    else:                                                                                                               #invalid input handling
+        print("invalid operation type!")
+        calculator()    
 
-if operation_type == "Addition" or operation_type == "addition":
-    print("operation selected: Addition")
-    print()
-    base_number = int(input("what is the base number (number to add to or subtract from): "))
-    add_number = int(input("what is the number to add? (enter '0' to skip)" ))
-    sub_number = int(input("what is the number to subtract? (enter '0' to skip)" ))
-    result = base_number + add_number - sub_number
-    print("base:", base_number, "add:", add_number,"sub:", sub_number, "=", result)
-elif operation_type == "Exponents" or operation_type == "exponents" or operation_type == "Exponent" or operation_type == "exponent":
-    print("operation selected: Exponent")
-    base_number = int(input("what is the base number (number to raise): "))
-    exponent_number = int(input("what is the power to raise the base to?"))
-    result = pow(base_number, exponent_number)
-    print("base:", base_number, "exp:", exponent_number, "=", result)
-elif operation_type == "both" or operation_type == "Both":
-    print()
-    print("----Addition----")
-    print()
-    base_number = int(input("what is the base number (number to add to or subtract from): "))
-    add_number = int(input("what is the number to add? (enter '0' to skip)" ))
-    sub_number = int(input("what is the number to subtract? (enter '0' to skip)" ))
-    result = base_number + add_number - sub_number
-    print("base:", base_number, "add:", add_number,"sub:", sub_number, "=", result)
-    print()
-    print("----Exponents----")
-    print()
-    base_number = int(input("what is the base number (number to raise): "))
-    exponent_number = int(input("what is the power to raise the base to?"))
-    result = pow(base_number, exponent_number)
-    print("base:", base_number, "exp:", exponent_number, "=", result)
-else:
-    print("invalid operation type! Please enter Addition, Exponents, or Both after restarting the program.")
+#----define a function to restart the calculator----
+def repeat_calculator():
+    again = input("would you like to use the calculator again? (y/n) ")
+    if again.lower() == "y":
+        print()
+        print("Restarting...")
+        print()
+        print()
+        print()
+        calculator()
+    elif again.lower() == "n":
+        print("calculator closed.")
+    else:
+        print("invalid response!")
+        repeat_calculator()
+
+#----run the calculator----
+calculator()
