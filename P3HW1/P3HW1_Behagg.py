acad0be6c98f0a42841dd3b6e6eb27ba3d74c1c4
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-09 14:20:08 +0000

    made some edits and tested the program for P3HW1 and added a screenshot

--- P3HW1/P3HW1_Behagg.py
+++ P3HW1/P3HW1_Behagg.py
@@ -1,20 +1,21 @@
-# I was supposed to put a comment here
-# My Last Name
+#Parker Behagg
+#3/9/26
+#P3HW1
 # This program takes a number grade , determines average and displays letter grade for average.
 # Enter grades for six modules
-mod_1 = input('Enter grade for Module 1: ')
-mod_2 = input('Enter grade for Module 2: ')
-mod_3 = input('Enter grade for Module 3: ')
-mod_4 = input('Enter grade for Module 4: ')
-mod_5 = input('Enter grade for Module 5: ')
-mod_6 = input('Enter grade for Module 6: ')
+mod_1 = float(input('Enter grade for Module 1: '))
+mod_2 = float(input('Enter grade for Module 2: '))
+mod_3 = float(input('Enter grade for Module 3: '))
+mod_4 = float(input('Enter grade for Module 4: '))
+mod_5 = float(input('Enter grade for Module 5: '))
+mod_6 = float(input('Enter grade for Module 6: '))
 # add grades entered to a list
 grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
 # TO DO: determine lowest, highest , sum and average for grades
 low = min(grades)
 high = max(grades)
-sum = sum(grades)
-avg = sum / len(grades)
+total = sum(grades)
+avg = total / len(grades)
 # determine letter grade for average
 if avg >= 90:
     print('Your grade is: A')
