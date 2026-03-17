cadf3f42adc4d3e962bd46ed07b07fcc22014d5e
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-17 10:32:45 -0400

    misc changes

--- P4LAB2/P4LAB2_Behagg.py
+++ P4LAB2/P4LAB2_Behagg.py
@@ -11,4 +11,11 @@ while yn.lower() == "yes":
         if integer > 0:
             result = input_from_user*integer
             print(f"{input_from_user} * {integer} = {result}")
+        elif input_from_user > 0:
+            print("no negative numbers please")
+        elif input_from_user == 0:
+            print("please enter a number greater than 0")
     yn = input("you ld you like to run the program again? (yes/no): ")
+    print()
+    print("exiting...")
+    print("InputInterrupt")
